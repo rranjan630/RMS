54f56d5bdebc36a44c223730828463daeac1ea7f
rranjan630 <rranjan630@gmail.com>
2016-05-16 04:21:15 -0700

    Creating RMS project.

---
+++ src/rms_project/Test.java
@@ -0,0 +1,13 @@
+package rms_project;
+
+public class Test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Employee emp = new Employee();
+		emp.setId(12);
+		emp.setName("rajesh");
+		System.out.println("id :"+emp.getId()+", name : "+emp.getName());
+	}
+
+}
